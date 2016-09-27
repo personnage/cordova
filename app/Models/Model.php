05b848f0073ca7a09b29c418e21ce8a8d02e3785
Lyubaev Kirill <lubaevka@gmail.com>
2016-09-27 18:38:05 +0300

    Fix base model.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,7 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
 class Model extends BaseModel
 {
-    //
+    protected static function boot()
+    {
+        parent::boot();
+
+        //
+    }
 }
