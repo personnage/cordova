a17061be16891f14891b7c5784d071bc27d2bd8f
Lyubaev Kirill <lubaevka@gmail.com>
2016-09-27 18:37:47 +0300

    New Photo model

---
+++ app/Models/Photo.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Photo extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'provider', 'extern_id', 'location',
+    ];
+
+    /**
+     * Get the user that owns the photo.
+     *
+     * @return [type] [description]
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the tags for the post.
+     */
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    /**
+     * Set the user's first name.
+     *
+     * @param  array  $value
+     * @return void
+     */
+    public function setLocationAttribute(array $value)
+    {
+        list($latitude, $longitude) = array_values($value);
+
+        $this->attributes['location'] = DB::raw(
+            "ST_GeographyFromText('SRID=4326;POINT({$latitude} {$longitude})')"
+        );
+    }
+}
